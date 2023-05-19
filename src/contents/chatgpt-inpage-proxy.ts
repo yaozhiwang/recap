2d784fbec55314eba97d498b911249e89d7d0b08
Yaozhi Wang <wangyaozhiyz@gmail.com>
2023-05-19 19:49:56 +0800

    remove tip panel on chatgpt

--- src/contents/chatgpt-inpage-proxy.ts
+++ src/contents/chatgpt-inpage-proxy.ts
@@ -6,20 +6,6 @@ export const config: PlasmoCSConfig = {
   matches: ["https://chat.openai.com/*"]
 }
 
-function injectTip() {
-  const div = document.createElement("div")
-  div.innerText = "Please keep this tab open, now you can go back"
-  div.style.position = "fixed"
-  // put the div at right top of page
-  div.style.top = "0"
-  div.style.right = "0"
-  div.style.zIndex = "50"
-  div.style.padding = "10px"
-  div.style.margin = "10px"
-  div.style.border = "1px solid"
-  document.body.appendChild(div)
-}
-
 async function main() {
   chrome.runtime.onMessage.addListener(
     async (message, sender, sendResponse) => {
@@ -30,7 +16,6 @@ async function main() {
   )
   if ((window as any).__NEXT_DATA__) {
     await chrome.runtime.sendMessage({ name: MessageNames.ProxyTabReady })
-    injectTip()
   }
 }
 
